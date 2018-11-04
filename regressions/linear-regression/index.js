f04473704d3af1f694c1f81974e1c565670b15e9
ghostindashell <mahjouri.saamahn@gmail.com>
2018-11-04 15:31:28 -0600

    first commit

---
+++ regressions/linear-regression/index.js
@@ -0,0 +1,33 @@
+require('@tensorflow/tfjs-node');
+
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('../load-csv');
+const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
+
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
+    shuffle: true,
+    splitTest: 120,
+    dataColumns: ['horsepower', 'weight', 'displacement', 'acceleration', 'cylinders'],
+    labelColumns: ['mpg']
+});
+
+const regression = new LinearRegression(features, labels, {
+    learningRate: 0.08,
+    iterations: 100,
+    batchSize: 10
+});
+
+regression.train();
+const r2 = regression.test(testFeatures, testLabels);
+
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration #',
+    yLabel: 'Mean Squared Error'
+});
+
+console.log('R2 is', r2);
+
+regression.predict([[120, 2, 380, 14, 6]]).print();
---
+++ regressions/logistic-regression/index.js
@@ -0,0 +1,36 @@
+require('@tensorflow/tfjs-node');
+
+const f = require('@tensorflow/tfjs');
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
+
+const { features, labels, testFeatures, testLabels } = loadCSV(
+    '../data/cars.csv',
+  {
+    dataColumns: ['horsepower', 'displacement','weight'],
+    labelColumns: ['passedemissions'],
+    shuffle: true,
+    splitTest: 50,
+    converters: {
+      passedemissions: (value) => {
+        return value === 'TRUE' ? 1 : 0;
+      }
+    } 
+  }
+);
+
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 10,
+  decisionBoundary: 0.5
+});
+
+regression.train();
+
+console.log(regression.test(testFeatures, testLabels));
+
+plot({
+    x: regression.costHistory.reverse()
+});
