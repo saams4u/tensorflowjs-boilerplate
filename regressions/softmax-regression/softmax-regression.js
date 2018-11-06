1f170886a97138e2ed958537f7421e9864c1c7bf
ghostindashell <mahjouri.saamahn@gmail.com>
2018-11-05 21:25:12 -0600

    added mnist and memory fix

--- regressions/softmax-regression/softmax-regression.js
+++ regressions/softmax-regression/softmax-regression.js
@@ -2,47 +2,48 @@ const tf = require('@tensorflow/tfjs');
 const _ = require('lodash');
 
 class SoftmaxRegression {
-    constructor(features, labels, options) {
-      this.features = this.processFeatures(features);
-      this.labels = tf.tensor(labels);
-      this.costHistory = [];
+  constructor(features, labels, options) {
+    this.features = this.processFeatures(features);
+    this.labels = tf.tensor(labels);
+    this.costHistory = [];
 
-      this.options = Object.assign(
-        { learningRate: 0.1, iterations: 1000, decisionBoundary: 0.5 }, 
-        options
-      );
+    this.options = Object.assign(
+      { learningRate: 0.1, iterations: 1000, decisionBoundary: 0.5 }, 
+      options
+    );
 
-      this.weights = tf.zeros([this.features.shape[1], this.labels.shape[1]]);
-    }
+    this.weights = tf.zeros([this.features.shape[1], this.labels.shape[1]]);
+  }
 
-    gradientDescent(features, labels) {
-      const currentGuesses = features
-        .matMul(this.weights)
-        .softmax();
-      const differences = currentGuesses.sub(labels);
+  gradientDescent(features, labels) {
+    const currentGuesses = features
+      .matMul(this.weights)
+      .softmax();
+    const differences = currentGuesses.sub(labels);
 
-      const slopes = features
-        .transpose()
-        .matMul(differences)
-        .div(features.shape[0]);
+    const slopes = features
+      .transpose()
+      .matMul(differences)
+      .div(features.shape[0]);
 
-      this.weights = this.weights
-        .sub(slopes.mul(this.options.learningRate));
-    }
+    return this.weights
+      .sub(slopes.mul(this.options.learningRate));
+  }
 
-    train() {
-      const batchQuantity = Math.floor(
-        this.features.shape[0] / this.options.batchSize
-      );
+  train() {
+    const batchQuantity = Math.floor(
+      this.features.shape[0] / this.options.batchSize
+    );
 
-      for (let i = 0; i < this.options.iterations; i++) {
-        for (let j = 0; j < batchQuantity; j++) {
-          const startIndex = j * this.options.batchSize;
-          const { batchSize } = this.options;
+    for (let i = 0; i < this.options.iterations; i++) {
+      for (let j = 0; j < batchQuantity; j++) {
+        const startIndex = j * this.options.batchSize;
+        const { batchSize } = this.options;
 
+        this.weights = tf.tidy(() => {
           const featureSlice = this.features.slice(
-              [startIndex, 0], 
-              [batchSize, -1]
+            [startIndex, 0], 
+            [batchSize, -1]
           );
 
           const labelSlice = this.labels.slice(
@@ -50,70 +51,78 @@ class SoftmaxRegression {
             [batchSize, -1]
           );
 
-          this.gradientDescent(featureSlice, labelSlice);
-        }
-
-        this.recordCost();
-        this.updateLearningRate();
-      }    
-    }
-
-    predict(observations) {
-      return this.processFeatures(observations)
-        .matMul(this.weights)
-        .softmax() 
-        .argMax(1);
-    }
-
-    test(testFeatures, testLabels) {
-      const predictions = this.predict(testFeatures);
-      testLabels = tf.tensor(testLabels).argMax(1);
-      
-      const incorrect = predictions
-        .notEqual(testLabels)
-        .sum()
-        .get();
-
-      return (predictions.shape[0] - incorrect) / predictions.shape[0];
-    }
-
-    processFeatures(features) {
-      features = tf.tensor(features);
-
-      if (this.mean && this.variance) {
-        features = features
-          .sub(this.mean)
-          .div(this.variance.pow(0.5));
-      } else {
-        features = this.standardize(features);
+          return this.gradientDescent(featureSlice, labelSlice);
+        });
       }
 
-      features = tf
-        .ones([features.shape[0], 1])
-        .concat(features, 1);
+      // debugger;
+      this.recordCost();
+      this.updateLearningRate();
+    }    
+  }
 
-      return features;
+  predict(observations) {
+    return this.processFeatures(observations)
+      .matMul(this.weights)
+      .softmax() 
+      .argMax(1);
+  }
+
+  test(testFeatures, testLabels) {
+    const predictions = this.predict(testFeatures);
+    testLabels = tf.tensor(testLabels).argMax(1);
+    
+    const incorrect = predictions
+      .notEqual(testLabels)
+      .sum()
+      .get();
+
+    return (predictions.shape[0] - incorrect) / predictions.shape[0];
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features);
+    // debugger;
+    if (this.mean && this.variance) {
+      features = features
+        .sub(this.mean)
+        .div(this.variance.pow(0.5));
+    } else {
+      features = this.standardize(features);
     }
 
-    standardize(features) {
-      const { mean, variance } = tf.moments(features, 0);
+    features = tf
+      .ones([features.shape[0], 1])
+      .concat(features, 1);
 
-      this.mean = mean;
-      this.variance = variance;
+    return features;
+  }
 
-      return features
-        .sub(mean)
-        .div(variance.pow(0.5));
-    }
+  standardize(features) {
+    const { mean, variance } = tf.moments(features, 0);
 
-    recordCost() {
+    const filler = variance.cast('bool').logicalNot().cast('float32');
+
+    this.mean = mean;
+    this.variance = variance.add(filler);
+
+    return features
+      .sub(mean)
+      .div(this.variance.pow(0.5));
+  }
+
+  recordCost() {
+    const cost = tf.tidy(() => {
+      //debugger;
       const guesses = this.features
-        .matMul(this.weights)
-        .softmax();
+      .matMul(this.weights)
+      .softmax();
 
       const termOne = this.labels
         .transpose()
-        .matMul(guesses.log());
+        .matMul(guesses
+        .add(1e-7) // Add a constant to avoid log(0)
+        .log()); 
 
       const termTwo = this.labels
         .mul(-1)
@@ -123,28 +132,31 @@ class SoftmaxRegression {
           guesses
             .mul(-1)
             .add(1)
+            .add(1e-7) // Add a constant to avoid log(0)
             .log()
         );
 
-      const cost = termOne.add(termTwo)
+      return termOne
+        .add(termTwo)
         .div(this.features.shape[0])
         .mul(-1)
         .get(0, 0);
+    });
 
-      this.costHistory.unshift(cost);
+    this.costHistory.unshift(cost);
+  }
+
+  updateLearningRate() {
+    if (this.costHistory.length < 2) {
+      return;
     }
 
-    updateLearningRate() {
-      if (this.costHistory.length < 2) {
-        return;
-      }
-
-      if (this.costHistory[0] > this.costHistory[1]) {
-        this.options.learningRate /= 2;
-      } else {
-        this.options.learningRate *= 1.05;
-      }
+    if (this.costHistory[0] > this.costHistory[1]) {
+      this.options.learningRate /= 2;
+    } else {
+      this.options.learningRate *= 1.05;
     }
+  }
 }
 
 module.exports = SoftmaxRegression;
